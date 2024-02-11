d401e030fcd45670b9b76963d595d346c90ca81f
Joel Birch <joeldbirch@gmail.com>
2024-02-12 09:54:03 +1100

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="nx-17-react-interview" />
+    </div>
+  );
+}
+
+export default App;
