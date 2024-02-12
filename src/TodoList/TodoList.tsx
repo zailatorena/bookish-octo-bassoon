3f714bc8fbfac569bcb195248c85c63faa865451
Joel Birch <joeldbirch@gmail.com>
2024-02-12 12:41:48 +1100

    setup problem.

--- src/TodoList/TodoList.tsx
+++ src/TodoList/TodoList.tsx
@@ -1,104 +1,30 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
-type Status = `error` | `success` | `loading`
+const TodoList = () => {
+  const [items, setItems] = useState(['Buy milk', 'Buy bread'])
 
-type BaseTodoListProps = {
-  items?: string[] | null
-  setItems: React.Dispatch<React.SetStateAction<string[]>>
-  errorMessage?: string
-  status?: Status
-}
-
-type TodoListErroredProps = BaseTodoListProps & {
-  errorMessage: string
-  status: `error`
-  items?: never
-}
-
-type TodoListSuccessProps = BaseTodoListProps & {
-  status: `success`
-  items: string[]
-}
-
-type TodoListLoadingProps = BaseTodoListProps & {
-  status: `loading`
-}
-
-type TodoListNoStatusProps = BaseTodoListProps & {
-  status?: never
-  errorMessage?: never
-}
-
-type TodoListProps =
-  | TodoListErroredProps
-  | TodoListSuccessProps
-  | TodoListLoadingProps
-  | TodoListNoStatusProps
-
-const TodoList = ({ items, setItems, status, errorMessage }: TodoListProps) => {
-  const [newItem, setNewItem] = useState(``)
-  const initRef = useRef(false)
-
-  const inputRef = useRef<HTMLInputElement>(null)
-
+  // pretend this is a side effect that adds a new task on mount
   useEffect(() => {
-    if (initRef.current) return
-    setItems((items) => [...items, 'Some other task'])
-    initRef.current = true
-  }, [setItems])
+    setItems(['Some other task'])
+  })
 
-  const handleClick = (todo = `new todo`) => {
-    if (!todo) return
-    setItems((items) => [...items, todo])
-    setNewItem(``)
-    inputRef.current?.focus()
+  // make this work first…
+  const handleClick = () => {
+    items.push('Brush teeth')
+    setItems(items)
   }
 
-  const isTouched = newItem !== ``
+  // …then allow custom tasks to be added via an input field
+  // instead of hardcoding the task in the handleClick function
 
   return (
     <div>
-      <div>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-          }}
-        >
-          <div className={`flex gap-2 m-4`}>
-            <input
-              ref={inputRef}
-              className={`p-2 rounded`}
-              type="text"
-              placeholder="new task"
-              value={newItem}
-              onChange={(e) => setNewItem(e.target.value)}
-            />
-            <button
-              disabled={isTouched ? false : true}
-              className={isTouched ? `cursor-not-allowed opacity-50` : ``}
-              onClick={() => handleClick(newItem)}
-            >
-              Add Task
-            </button>
-          </div>
-        </form>
-      </div>
-
-      {errorMessage && (
-        <div className={`border bg-red-500/20 border-red-500 my-4 p-2`}>
-          Error: {errorMessage}
-          <br />
-          Status: {status}
-        </div>
-      )}
-
-      {items && items.length > 0 && (
-        <ul>
-          {items.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      )}
+      <a onClick={handleClick}>Add Task</a>
+      <ul>
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
     </div>
   )
 }
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
 import './app.css'
 import TodoList from '../TodoList/TodoList'
 import Layout from '../Layout'
 
 function App() {
-  const [items, setItems] = useState(['Buy milk', 'Buy bread'])
-
   return (
     <Layout
       complementary={
         <p>
-          You have <strong>{items.length}</strong> total tasks.
+          You have <strong>×</strong> total tasks.
         </p>
       }
     >
-      <TodoList items={items} setItems={setItems} status="success" />
+      <TodoList />
     </Layout>
   )
 }
