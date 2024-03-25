873340842085ffb901b870c777a272d2a5ba851e
Joel Birch <joeldbirch@gmail.com>
2024-03-25 16:50:52 +1100

    Clarify comments.

--- src/TodoList/TodoList.tsx
+++ src/TodoList/TodoList.tsx
@@ -3,18 +3,18 @@ import { useEffect, useState } from 'react'
 const TodoList = () => {
   const [items, setItems] = useState(['Buy milk', 'Buy bread'])
 
-  // pretend this is a side effect that adds a new task on mount
+  // The list should contain 3 tasks after the component mounts. Two tasks are hardcoded in the initial state (above) and the third task is added via the useEffect hook.
   useEffect(() => {
     setItems(['Some other task'])
   })
 
-  // make this work first…
+  // When the user clicks the "Add Task" link, a new (hardcoded) task should be added to the list.
   const handleClick = () => {
     items.push('Brush teeth')
     setItems(items)
   }
 
-  // …then allow custom tasks to be added via an input field
+  // Next, allow custom tasks to be added via an input field
   // instead of hardcoding the task in the handleClick function
 
   return (
@@ -28,5 +28,8 @@ const TodoList = () => {
     </div>
   )
 }
+
+// Finally, make sure all the tests in arc/app/app.spec.tsx are passing.
+// Add missing functionality until they do.
 
 export default TodoList
